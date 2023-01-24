51141fa73ace3ef0af8f5bc9199b8e104e5bd66e
nenes <nesmon2108@protonmail.com>
2023-01-24 13:32:25 +0100

    Fix and start expense

--- shoppingapp/src/App.js
+++ shoppingapp/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const expense = [
+    { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: Date(2020, 7, 14) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: Date(2021, 2, 12) },
+    { id: 'e3', title: 'Car Insurance', amount: 294.67, date: Date(2021, 2, 28) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: Date(2021, 5, 12) },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {expense.map((el, index) => (
+        <div key={index}>
+          <h2>{el.title}</h2>
+          <div>{el.amount}</div>
+          <div>{el.date}</div>
+        </div>
+      ))}
     </div>
   );
 }
+
 
 export default App;
